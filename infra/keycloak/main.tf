6c4eb658d6f9bd5b88bb895d22520b5f0352f1cd
Yuxiang-Huang <yuxiangh142857@gmail.com>
2026-03-04 18:50:49 -0500

    feat(iac): manage keycloak auth flow

--- infra/keycloak/main.tf
+++ infra/keycloak/main.tf
@@ -1,6 +1,7 @@
 resource "keycloak_realm" "labrador" {
   realm                       = "labrador"
   default_signature_algorithm = "RS256"
+  remember_me                 = true
 }
 
 # Client that handles OIDC login in OpenBao
@@ -27,4 +28,28 @@ resource "keycloak_openid_client" "openbao" {
   service_accounts_enabled = true
 
   frontchannel_logout_enabled = true
+}
+
+resource "keycloak_authentication_flow" "auto_link_ldap_users" {
+  realm_id    = keycloak_realm.labrador.id
+  alias       = "Auto-link LDAP users"
+  description = "Actions taken after first broker login with identity provider account, which is not yet linked to any Keycloak account"
+}
+
+
+# Results in "Invalid username or password." error when this is disabled
+resource "keycloak_authentication_execution" "create_user_if_unique" {
+  realm_id          = keycloak_realm.labrador.id
+  parent_flow_alias = "Auto-link LDAP users"
+  authenticator     = "idp-create-user-if-unique"
+  requirement       = "ALTERNATIVE"
+  priority          = 0
+}
+
+resource "keycloak_authentication_execution" "auto_set_existing_user" {
+  realm_id          = keycloak_realm.labrador.id
+  parent_flow_alias = "Auto-link LDAP users"
+  authenticator     = "idp-auto-link"
+  requirement       = "ALTERNATIVE"
+  priority          = 1
 }
