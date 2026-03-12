59f8a2b484718f3ba06136095783e4df136892c5
Yuxiang-Huang <yuxiangh142857@gmail.com>
2026-03-11 21:54:28 -0400

    feat(iac): add display name for Keycloak realm

--- infra/keycloak/main.tf
+++ infra/keycloak/main.tf
@@ -1,6 +1,7 @@
 # Labrador realm
 resource "keycloak_realm" "labrador" {
   realm                       = "labrador"
+  display_name                = "Labrador"
   default_signature_algorithm = "RS256"
   remember_me                 = true
 }
