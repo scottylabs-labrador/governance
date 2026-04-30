a332ec8086afa53deec34c4a11c8c59dac51f26f
Yuxiang-Huang <yuxiangh142857@gmail.com>
2026-04-30 16:43:17 -0400

    feat(iac): add team repo permission

--- infra/github/teams.tf
+++ infra/github/teams.tf
@@ -1,4 +1,6 @@
 # For each team, create a team for all members and a sub-team for admins.
+# The admin team has admin permission on the team repositories.
+# The member team has write permission on the team repositories.
 
 resource "github_team" "teams" {
   for_each    = var.teams_data
@@ -12,6 +14,33 @@ resource "github_team" "admin_teams" {
   name           = "${each.value.name} Admins"
   privacy        = "closed" # Visible to all members of the organization
   parent_team_id = github_team.teams[each.key].id
+}
+
+# Team repositories
+locals {
+  team_repositories = merge([
+    for team_slug, team in var.teams_data : {
+      for repo in team.repos :
+      "${team_slug}:${repo}" => {
+        team_slug = team_slug
+        repo      = repo
+      }
+    }
+  ]...)
+}
+
+resource "github_team_repository" "team_repositories" {
+  for_each   = local.team_repositories
+  team_id    = github_team.teams[each.value.team_slug].id
+  repository = each.value.repo
+  permission = "push"
+}
+
+resource "github_team_repository" "team_admin_repositories" {
+  for_each   = local.team_repositories
+  team_id    = github_team.admin_teams[each.value.team_slug].id
+  repository = each.value.repo
+  permission = "admin"
 }
 
 # Team memberships
