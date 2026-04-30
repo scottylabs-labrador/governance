6b5430296d491b462749d8f3c69b4fd3bd66aa8b
Yuxiang-Huang <yuxiangh142857@gmail.com>
2026-04-29 22:25:14 -0400

    chore(linter): remove ty
    type: ignore comment doesn't play well with mypy and ty enabled

--- meta/linter/__init__.py
+++ meta/linter/__init__.py
@@ -7,4 +7,3 @@ def main() -> None:
     """For uv run lint to work."""
     subprocess.run(["ruff", "check"], check=True)  # noqa: S607
     subprocess.run(["mypy", "."], check=True)  # noqa: S607
-    subprocess.run(["ty", "check"], check=True)  # noqa: S607
